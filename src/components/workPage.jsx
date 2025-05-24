b9f9d19f5e252cc97176d799ac45c5007ec82f84
salimon victor <victorsalimon6@gmail.com>
2025-05-24 21:26:08 +0100

    updated work page by adding the weather app

--- src/components/workPage.jsx
+++ src/components/workPage.jsx
@@ -12,26 +12,27 @@ function WorkPage() {
         initial="hidden"
         whileInView="show"
         className="wrapper">
-        <Motion.h4
-          variants={textVariant(0.3)}
-        >Blog</Motion.h4>
-        <Motion.h5
-          variants={textVariant(0.4)}
-          style={{ color: "#e90f1f" }} className="a-1 pointer"><a href="https://blog-template-vjaystorms.onrender.com">A simplistic template example.</a></Motion.h5>
 
         <Motion.h4
-          variants={textVariant(0.5)}
+          variants={textVariant(0.3)}
         >Landing Page</Motion.h4>
         <Motion.h5
-          variants={textVariant(0.6)}
+          variants={textVariant(0.4)}
           style={{ color: "#e90f1f" }} className="a-2 pointer"><a href="https://react-landing-page-taiilwind-css-framer-motion.vercel.app/">Modern Landing Page | React,Tailwindcss and Framer motion</a></Motion.h5>
 
         <Motion.h4
-          variants={textVariant(0.7)}
+          variants={textVariant(0.5)}
         >Weather App</Motion.h4>
         <Motion.h5
-          variants={textVariant(0.8)}
+          variants={textVariant(0.6)}
           style={{ color: "#e90f1f" }} className="a-2 pointer"><a href="https://weather-app-three-chi-54.vercel.app/">Weather Dashboard | React, React Query, API Integration</a></Motion.h5>
+
+        <Motion.h4
+          variants={textVariant(0.7)}
+        >Blog</Motion.h4>
+        <Motion.h5
+          variants={textVariant(0.8)}
+          style={{ color: "#e90f1f" }} className="a-1 pointer"><a href="https://blog-template-vjaystorms.onrender.com">A simplistic template example.</a></Motion.h5>
 
       </Motion.div>
     </div>
